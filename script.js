c1e4f629f7d3acc972b74345afc8ed7794e28af9
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-04-16 13:04:40 +0530

    Conversion of number to string

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-// Basic Printing on console
-console.log("Animesh");
+// // Basic Printing on console
+// console.log("Animesh");
 
-// Basic Printing of Alert
-alert("Hii myself Animesh");
+// // Basic Printing of Alert
+// alert("Hii myself Animesh");
+
+// Conversion of the number to string 
+let valueNumber = 8;
+let valueString = String(valueNumber);
+
+console.log(valueString);
+console.log(typeof "valueNumber");
