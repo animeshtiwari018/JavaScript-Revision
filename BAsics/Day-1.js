b19d2db3e2b23b90978a87c84710debf3f1f67c3
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-04-16 15:08:27 +0530

    Use of operator, concatation, postfix and prefix incremental operator

---
+++ BAsics/Day-1.js
@@ -0,0 +1,26 @@
+// ******************Operations*************************
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2); /*used as exponant operator ( 2*3 = 8; 3 is power of 2) /*
+console.log(2/2);
+console.log(2%2);
+
+// output of this will be 122 because 1 is also get converted in string becuase of two strings are there and it get concanate with them
+console.log(1 + "2" + "2");
+console.log("1" + 2 + "2"); /* output: 122 */
+console.log(1 + 2 + "2");   /* output: 32 (because of operator presedence*/
+
+// increment operator 
+//  ****Prefix operation****
+let value = 50;
+console.log(++value);
+console.log(value);
+// it increase first and then use
+
+//  ****Postfix operation****
+// it increase later 
+console.log(value++);
+console.log(value);
+
