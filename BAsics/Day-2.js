cfb81ac9875af8521ba190530494fe3e5e24d50a
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-04-17 16:32:30 +0530

    some basic methods yet useful

--- BAsics/Day-2.js
+++ BAsics/Day-2.js
@@ -18,3 +18,30 @@ console.log(newString.replace("string", "String"));
 
 let isUSer = "  Animesh     "
 console.log(isUSer.trim());
+
+
+// ***********Number and Maths***************
+const newNumber = new Number(100);
+console.log(newNumber); 
+console.log(newNumber.toString());
+console.log(newNumber.toString().length);
+
+// *****toFixed*****
+const anotherNumber = 100.345;
+console.log(anotherNumber.toFixed());
+console.log(anotherNumber.toFixed(1));
+// so toFixed is used to round off the number after the decimeal value we have to pass in the argument how much number we need after the decimal value 
+// **Important:- toFixed always return String value after the conversion
+const firstNum = 100.456;
+const y = firstNum.toFixed(); 
+// console.log(typeof y);  /* String */
+console.log(Number(y));
+console.log(typeof y); 
+console.log(typeof firstNum); /* Number */
+
+const finalResult = Number((100.67).toFixed());
+console.log(finalResult);
+console.log(typeof finalResult);
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));
