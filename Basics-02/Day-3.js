dddb31f156914963aa950bb661b61cd78a246ce3
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-04-18 20:39:01 +0530

    Use of push() and splice() method in te array

--- Basics-02/Day-3.js
+++ Basics-02/Day-3.js
@@ -1,5 +1,13 @@
 // const myArr = [10, 20, 30, 40];
 // console.log(myArr);
 
-// const newArr = new Array(10, 20, 30, 40);
-// console.log(newArr);
+const newArr = new Array(10, 20, 30, 40);
+console.log(newArr);
+
+// **in Push() method we can only add element in the end of the array 
+newArr.push(5);
+console.log(newArr);
+
+// **For add element in the particular index we use 
+newArr.splice(2, 0, 50);
+console.log(newArr);
