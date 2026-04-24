c68552d2ae46ae554ea53df67610c68794bed1af
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-04-24 20:41:38 +0530

    concat of array

---
+++ Basics-02/Day_4.js
@@ -0,0 +1,14 @@
+const DC_hero = ["superman", "flash", "batman"];
+const Marvel_hero = ["thor", "irconman", "spiderman"];
+
+// console.log(DC_hero);
+// console.log(DC_hero.push(Marvel_hero));
+
+
+// ***This is not the right way to add two array
+// DC_hero.push(Marvel_hero);
+// console.log(DC_hero);
+
+
+// DC_hero.concat(Marvel_hero);
+// console.log(DC_hero);
