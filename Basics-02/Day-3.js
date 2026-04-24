d1f6b7e72044bc20810550b8cac3271e14d5d64d
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-04-24 20:38:24 +0530

    End of the day

--- Basics-02/Day-3.js
+++ Basics-02/Day-3.js
@@ -5,9 +5,17 @@ const newArr = new Array(10, 20, 30, 40);
 console.log(newArr);
 
 // **in Push() method we can only add element in the end of the array 
-newArr.push(5);
-console.log(newArr);
+// newArr.push(5);
+// console.log(newArr);
 
 // **For add element in the particular index we use 
-newArr.splice(2, 0, 50);
-console.log(newArr);
+// newArr.splice(2, 0, 50);
+// console.log(newArr);
+
+
+// **Difference between slice() or splice()
+
+let arr = [1,2,3,4,5];
+console.log()
+
+console.log(newArray);
