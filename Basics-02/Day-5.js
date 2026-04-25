f1974ff36a60fb29dd147a765fe8b4ceab943859
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-04-25 12:24:46 +0530

    understandng of object one by one

--- Basics-02/Day-5.js
+++ Basics-02/Day-5.js
@@ -1,14 +1,25 @@
-// const user = {
-//   name: "Animesh Tiwari",
-//   "full name": "Animesh Tiwari",
-//   location: "Jaipur",
-//   isLoggedIn: false,
-//   age: 19,
-//   email: "animeshtiwari178@gmail.com",
-//   lastLoggedIn: ["Monday", "Tuesday"],
-// };
+const mySym = Symbol("key");
 
-// console.log(user.email);
-// console.log(user.age);
-// // now it is not possible to accesss full name using . so here we use 
-// console.log(user["full name"])
+const user = {
+  name: "Animesh Tiwari",
+  "full name": "Animesh Tiwari",
+  location: "Jaipur",
+  isLoggedIn: false,
+  age: 19,
+  email: "animeshtiwari178@gmail.com",
+  lastLoggedIn: ["Monday", "Tuesday"],
+  [mySym]: "myKey",
+};
+
+console.log(user.email);
+console.log(user.age);
+console.log(user[mySym]);
+// // now it is not possible to accesss full name using . so here we use
+console.log(user["full name"]);
+
+user.email = "animesh@microsoft.com"
+console.log(user)
+Object.freeze(user)
+
+user.email = "animesh@google.com";
+console.log(user)
