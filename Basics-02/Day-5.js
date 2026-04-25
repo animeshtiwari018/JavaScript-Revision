faa4ce5b02595ff65ec2f89e5fd1060359ae77c3
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-04-25 12:12:24 +0530

    Object declare and use of bracked access method of objects

---
+++ Basics-02/Day-5.js
@@ -0,0 +1,14 @@
+// const user = {
+//   name: "Animesh Tiwari",
+//   "full name": "Animesh Tiwari",
+//   location: "Jaipur",
+//   isLoggedIn: false,
+//   age: 19,
+//   email: "animeshtiwari178@gmail.com",
+//   lastLoggedIn: ["Monday", "Tuesday"],
+// };
+
+// console.log(user.email);
+// console.log(user.age);
+// // now it is not possible to accesss full name using . so here we use 
+// console.log(user["full name"])
