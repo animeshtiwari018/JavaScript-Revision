034007c33a64bbc4323b434ebf260140d4fe3709
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-04-25 14:52:48 +0530

    use of object in function

--- Basics-02/Day-5.js
+++ Basics-02/Day-5.js
@@ -1,25 +1,41 @@
-const mySym = Symbol("key");
+// const mySym = Symbol("key");
 
-const user = {
-  name: "Animesh Tiwari",
-  "full name": "Animesh Tiwari",
-  location: "Jaipur",
-  isLoggedIn: false,
-  age: 19,
-  email: "animeshtiwari178@gmail.com",
-  lastLoggedIn: ["Monday", "Tuesday"],
-  [mySym]: "myKey",
+// const user = {
+//   name: "Animesh Tiwari",
+//   "full name": "Animesh Tiwari",
+//   location: "Jaipur",
+//   isLoggedIn: false,
+//   age: 19,
+//   email: "animeshtiwari178@gmail.com",
+//   lastLoggedIn: ["Monday", "Tuesday"],
+//   [mySym]: "myKey",
+// };
+
+// console.log(user.email);
+// console.log(user.age);
+// console.log(user[mySym]);
+// // // now it is not possible to accesss full name using . so here we use
+// console.log(user["full name"]);
+
+// user.email = "animesh@microsoft.com"
+// console.log(user)
+// Object.freeze(user)
+
+// user.email = "animesh@google.com";
+// console.log(user)
+
+// ****Function using objects
+
+// this only add 1 value in num 1 = 20;
+const calculatePrice = function (num1) {
+  return num1;
 };
+console.log(calculatePrice(20, 40));
 
-console.log(user.email);
-console.log(user.age);
-console.log(user[mySym]);
-// // now it is not possible to accesss full name using . so here we use
-console.log(user["full name"]);
+// so this is called rest operator (...num) rest and spread both of them has same syntax
+const newCalculatePrice = function (...num1) {
+  return num1;
+};
+console.log(newCalculatePrice(20, 40));
 
-user.email = "animesh@microsoft.com"
-console.log(user)
-Object.freeze(user)
 
-user.email = "animesh@google.com";
-console.log(user)
