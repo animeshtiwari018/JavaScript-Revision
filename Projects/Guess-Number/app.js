5f271b41dfdcd8ab2ca8f6d18b74e5e798818aef
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-04-29 13:10:05 +0530

    Structure of js file is started a main main functions is declared.

--- Projects/Guess-Number/app.js
+++ Projects/Guess-Number/app.js
@@ -1,1 +1,43 @@
 const randomNum = parseInt(Math.random() * 20 + 1);
+
+const submit = document.querySelector("btn check");
+const reset = document.querySelector("btn again");
+const num = document.querySelector("number");
+const userInput = document.querySelector("guess");
+const messageGuess = document.querySelector("message");
+const Remaining = document.querySelector("score");
+const score = document.querySelector("highscore");
+
+let prevGuess = [];
+let newGuess = 1;
+let startGame = true;
+
+if (startGame) {
+  submit.addEventListener("click", function (e) {
+    const guess = parseInt(userInput.value);
+    validateGuess();
+  });
+}
+function validateGuess(guess) {
+  //
+}
+
+function checkGuess(guess) {
+  //
+}
+
+function displayGuess(guess) {
+  //
+}
+
+function dispalyMessage() {
+  //
+}
+
+function endGame() {
+  //
+}
+
+function newGame() {
+  //
+}
