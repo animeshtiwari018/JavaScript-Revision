771e3b38ad925721fde022cc6dca93766652cb86
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-05-02 14:24:09 +0530

    clear Timeout is done

--- DOM/app.js
+++ DOM/app.js
@@ -1,7 +1,14 @@
 //  ****setTimeout****
 
-const neww = setTimeout(function () {
-  alert("You are Noob");
-}, 2000);
+// const neww = setTimeout(function () {
+//   alert("You are Noob");
+// }, 2000);
 
-const clearNew = clearTimeout(neww);
+// const clearNew = clearTimeout(neww);
+
+const changeMe = function () {
+  document.querySelector("h2").innerHTML = "IG Bravo";
+};
+
+setTimeout(changeMe, 2000);
+clearTimeout(changeMe);
