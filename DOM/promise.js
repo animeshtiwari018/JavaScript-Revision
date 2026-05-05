c012d63704823378a06582740d7891f2eb7828bf
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-05-05 15:36:00 +0530

    Basic use of g=fetch() in api

--- DOM/promise.js
+++ DOM/promise.js
@@ -1,23 +1,30 @@
-new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    console.log("Async is done");
-    resolve(); /*we have to add this to run then */
-  }, 1000);
-}).then(function () {
-  console.log("Async 1 is resolved");
-});
+// new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     console.log("Async is done");
+//     resolve(); /*we have to add this to run then */
+//   }, 1000);
+// }).then(function () {
+//   console.log("Async 1 is resolved");
+// });
 
-const promiseThree = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    console.log("Async 3 is done");
-    resolve({
-      username: "Animesh Tiwari",
-      email: "animeshtiwari178@gmail.coms",
-    });
-  }, 1000);
-});
+// const promiseThree = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     console.log("Async 3 is done");
+//     resolve({
+//       username: "Animesh Tiwari",
+//       email: "animeshtiwari178@gmail.coms",
+//     });
+//   }, 1000);
+// });
 
-promiseThree.then(function (user) {
-  console.log("user is called");
-  console.log(user.username);
-});
+// promiseThree.then(function (user) {
+//   console.log("user is called");
+//   console.log(user.username);
+// });
+
+const URL = fetch(
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=",
+)
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log("Error", error))
