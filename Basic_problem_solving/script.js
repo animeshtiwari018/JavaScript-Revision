c08fea838230da7bf7bec8e67e8108b0837620ca
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-05-11 21:17:46 +0530

    done

--- Basic_problem_solving/script.js
+++ Basic_problem_solving/script.js
@@ -27,22 +27,22 @@
 
 // **** For loop working****
 
-class Solution {
-  studentGrade(marks) {
-    if (marks >= 90) {
-      console.log("Grade A");
-    } else if (marks >= 70) {
-      console.log("Grade B");
-    } else if (marks >= 50) {
-      console.log("Grade C");
-    } else if (marks >= 35) {
-      console.log("Grade D");
-    } else {
-      console.log("Fail");
-    }
-  }
-}
+// class Solution {
+//   studentGrade(marks) {
+//     if (marks >= 90) {
+//       console.log("Grade A");
+//     } else if (marks >= 70) {
+//       console.log("Grade B");
+//     } else if (marks >= 50) {
+//       console.log("Grade C");
+//     } else if (marks >= 35) {
+//       console.log("Grade D");
+//     } else {
+//       console.log("Fail");
+//     }
+//   }
+// }
 
-let sol = new Solution();
-sol.studentGrade(95);
+// let sol = new Solution();
+// sol.studentGrade(95);
 
