7575f363e364e4b2cdc357f0976ad27891e876c9
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-05-13 11:16:20 +0530

    Added two numbers

---
+++ Basic_problem_solving/script-1.js
@@ -0,0 +1,14 @@
+let userName = "animesh";
+console.log(userName);
+
+// ****Add two numbers****
+
+// function numberSum() {
+//     let sum = 0;
+//     let a = 1;
+//     let b = 2;
+//     sum = a + b;
+    
+//     return sum;
+// }
+// console.log(numberSum());
