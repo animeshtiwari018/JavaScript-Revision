72bad274eea51cf7fe822618487e78ebb251fc03
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-06-04 23:01:25 +0530

    Done some of the changes

---
+++ Mini_projects/Expense_tracker/app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const ExpenseName = document.getElementById("expenseName");
+  const ExpenseAmount = document.getElementById("expenseAmount");
+  const SubmitBtn = document.getElementById("submit");
+});
