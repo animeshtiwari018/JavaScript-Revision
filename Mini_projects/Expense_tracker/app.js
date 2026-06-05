4f5650cc0e971f9fe05b39bb2e9f8cf20ad82377
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-06-05 15:33:12 +0530

    Basic functioning is done now can add expense in the ul

--- Mini_projects/Expense_tracker/app.js
+++ Mini_projects/Expense_tracker/app.js
@@ -2,4 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const ExpenseName = document.getElementById("expenseName");
   const ExpenseAmount = document.getElementById("expenseAmount");
   const SubmitBtn = document.getElementById("submit");
+  const expenseList = document.getElementById("expense-list");
+  const totalAmountDisplay = document.getElementById("total-amount");
+
+  let expenses = [];
+  let totalAmount = calculateTotal();
+
+  SubmitBtn.addEventListener("click", (e) => {
+    const expenseValue = String(ExpenseName.value);
+    const expenseAmountValue = parseInt(ExpenseAmount.value);
+
+    const list = document.createElement("li");
+    list.classList.add("newLi");
+    list.textContent = `${expenseValue} - ${expenseAmountValue}`;
+    expenseList.appendChild(list);
+  });
+
+  function calculateTotal() {}
 });
